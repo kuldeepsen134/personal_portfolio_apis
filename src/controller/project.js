20e5f92106140d311a233ce00f79bd8dea4a9fce
Parkhya.itpark <parkhya.itparkh@parkhya.com>
2024-03-18 13:02:31 +0530

    Project single, project delete and project update API done

--- src/controller/project.js
+++ src/controller/project.js
@@ -48,3 +48,69 @@ exports.find = async (req, res) => {
     handleError(error.message, 400, res);
   }
 };
+
+exports.findOne = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const project = await Project.findOne({ _id: id });
+
+    handleResponse(res, project._doc, 200);
+  } catch (error) {
+    handleError(error.message, 400, res);
+  }
+};
+
+
+
+// For admin only
+exports.deleteOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findOne({ _id: id });
+
+    if (!project) {
+      handleError('Invailid project ID', 400, res)
+      return
+    }
+
+    await Project.deleteOne({ _id: project._id })
+
+    handleResponse(res, [], 'Project deleted successfully', 200);
+  } catch (error) {
+    handleError(error.message, 400, res);
+  }
+};
+
+
+
+// For admin only
+exports.updateOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { title, short_desc, description, github, liveURL } = req.body;
+
+    const files = req.files;
+
+    const video = files?.video?.map((file) => `/media/${file?.filename}`);
+
+    const photoes = files?.image?.map((file) => `/media/${file?.filename}`);
+
+    const data = { title, short_desc, description, github, liveURL, photoes, video, };
+
+
+
+    const project = await Project.findAndUpdateOne({ _id: id }, data, { new: true });
+
+    if (!project) {
+      handleError('Invailid project ID', 400, res)
+      return
+    }
+
+    handleResponse(res, [], 'Project has been updated successfully', 200);
+
+  } catch (error) {
+    handleError(error.message, 400, res);
+  }
+};
--- src/router/project.js
+++ src/router/project.js
@@ -6,6 +6,12 @@ var router = require('express').Router();
 module.exports = app => {
     router.post('/projects', multipleFileUploading, projects.create)
     router.get('/projects', projects.find)
+    router.delete('/projects/:id', projects.deleteOne)
+    router.get('/projects/:id', projects.findOne)
+
+    // router.patch('/projects/:id', projects.updateOne)
+
+
 
 
     app.use('/api', router);
